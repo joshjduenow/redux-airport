ea99753f9bef32684d845c231349aabe4c972ec0
Joshua Duenow <joshjduenow@gmail.com>
2023-11-28 14:36:54 -0600

    data store created

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
 
-/** TODO: import REDUX **/
-
-/** TODO: Add REDUCERS */
 
 /** TODO: Create store */
+
+const airlines = (state=[], action) => {
+
+    return state
+}
+
+const airlineStore = createStore(
+    combineReducers({
+        airlines
+    })
+)
 
 // Be sure to add the Provider! Just wrap App with it. Don't copy and paste from lecture, that will cause issues.
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <Provider store={airlineStore}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
