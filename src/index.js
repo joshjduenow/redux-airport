8701f6a3faa3402a39fd89f5fdc2a998f34a6fc3
Joshua Duenow <joshjduenow@gmail.com>
2023-11-28 14:29:02 -0600

    installed redux

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App/App";
 
 /** TODO: import REDUX **/
 
-
 /** TODO: Add REDUCERS */
-
 
 /** TODO: Create store */
 
-
-
 // Be sure to add the Provider! Just wrap App with it. Don't copy and paste from lecture, that will cause issues.
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
